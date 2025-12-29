db85d93c9ffc4f9efdae76642aad7d947d9fa9de
Bezaleel Elderoy <142726056+bez4leel@users.noreply.github.com>
2025-12-30 00:05:12 +0800

    Guess a random number

---
+++ GuessRandomNumber/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace GuessRandomNumber
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            bool gameRunning = true;
+
+            Random rnd = new Random();
+
+            int randomNumber = rnd.Next(1, 100);
+
+            int tries = 3;
+
+            while (gameRunning)
+            {
+                Console.Write($"Guess a number: ");
+                string userInput = Console.ReadLine();
+                int userGuess = 0;
+                bool isNumber = int.TryParse(userInput, out userGuess);
+                if (userGuess == randomNumber && isNumber)
+                {
+                    Console.WriteLine("Congratulations you guessed the right number!");
+                    gameRunning = false;
+                }
+                else
+                {
+                    tries--;
+                    Console.WriteLine($"Wrong you got {tries} more tries");
+                }
+
+                if (tries == 0)
+                {
+                    Console.WriteLine("Game over");
+                    Console.Write("Retry? (Y/N) :");
+                    string retry = Console.ReadLine();
+                    if (retry == "Y" || retry == "y")
+                    {
+                        Console.Clear();
+                        tries = 3;
+                    }
+                    else
+                    {
+                        gameRunning = false;
+                    }
+                }
+            }
+        }
+    }
+}
